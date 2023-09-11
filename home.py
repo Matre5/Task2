72b32700f57fec23cb31d23fe4f6178abbcfea70
Matre Aiguokhian <aigmatre5@gmail.com>
2023-09-11 23:27:44 +0100

    first commit

---
+++ home.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/api')
+def get_info():
+
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    if track != 'backend':
+        return jsonify({'Error': 'invalid track'}), 400
+
+    # getting the current date and time
+    current_day = datetime.datetime.now().strftime('%A')
+
+    # Getting the UTC time
+    utc_time = datetime.datetime.now().strftime("%Y-%m-%dT %H:%M:%SZ")
+
+
+    git_file = "https://github.com/Matre5/Task/blob/master/helloo.py"
+
+    git_repo = "https://github.com/Matre5/Task"
+
+
+    status_code = 200
+
+    result = {
+        'slack_name': slack_name,
+        'track': track,
+        'Day': current_day,
+        'UTC': utc_time,
+        'git_file': git_file,
+        'git_repo': git_repo,
+        'status_code': status_code
+    }
+
+    # response = jsonify(result)
+
+    # response.headers['Content-type'] = 'application/json'
+
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
